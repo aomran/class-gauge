5d0ee3e7d45c734743620a2ebb0c1078580aea89
Ahmed O <aomra015@gmail.com>
2014-09-12 18:20:40 -0400

    simplify models be removing vote model

--- app/models/lesson.js
+++ app/models/lesson.js
@@ -2,8 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  surveys: DS.hasMany('survey', { async: true }),
-  surveyCount: function(){
-  	return this.get('surveys.length');
-  }.property('surveys.length')
+  surveys: DS.hasMany('survey', { async: true })
 });
--- app/models/survey.js
+++ app/models/survey.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   timestamp: DS.attr('date'),
+  yesVotes: DS.attr('number'),
+  noVotes: DS.attr('number'),
   lesson: DS.belongsTo('lesson', { async: true }),
-  votes: DS.hasMany('vote', { async: true })
 });
