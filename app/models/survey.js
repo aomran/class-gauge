577b3329904521370db96ee18f44c9ca57a6eca7
Ahmed O <aomra015@gmail.com>
2014-09-13 14:38:44 -0400

    yes and no votes now default to 0

--- app/models/survey.js
+++ app/models/survey.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   timestamp: DS.attr('date'),
-  yesVotes: DS.attr('number'),
-  noVotes: DS.attr('number'),
+  yesVotes: DS.attr('number', {defaultValue: 0}),
+  noVotes: DS.attr('number', {defaultValue: 0}),
   lesson: DS.belongsTo('lesson', { async: true }),
 });
