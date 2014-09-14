7922a33f947e1c43d14a9a1f2ec17a41c72068b3
Ahmed O <aomra015@gmail.com>
2014-09-13 21:03:59 -0400

    extract ember chart into addon

--- Brocfile.js
+++ Brocfile.js
@@ -17,6 +17,4 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/chartjs/Chart.js');
-
 module.exports = app.toTree();
