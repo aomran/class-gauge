03f17fe3861671e785bb47a913da503d132be4bf
Ahmed O <aomra015@gmail.com>
2014-09-20 20:39:46 -0400

    clipboard component is just a button now

--- app/components/zero-clipboard.js
+++ app/components/zero-clipboard.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 /* global ZeroClipboard */
 
 export default Ember.Component.extend({
+	tagName: 'button',
+	attributeBindings: ['title', 'data-clipboard-text'],
+	title: 'Copy to clipboard',
 	didInsertElement: function () {
-	  new ZeroClipboard(this.$('button'));
-	}
+	  new ZeroClipboard(this.get('element'));
+	},
+	"data-clipboard-text": function(){
+		return this.get('text');
+	}.property('text')
 });
